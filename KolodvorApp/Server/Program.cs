8e1cbeb629492670c0d2d44e4fa1b2dc786e51e8
Tomislav Ogrinec <togrinec@conet.hr>
2023-05-23 19:07:26 +0200

    setup dependency injection

--- KolodvorApp/Server/Program.cs
+++ KolodvorApp/Server/Program.cs
@@ -1,36 +1,14 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using KolodvorApp.Server.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddEnvironmentVariables();
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.ConfigureServices(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
+app.Configure()
+    .MigrateDatabase()
+    .SeedData()
+    .Run();
