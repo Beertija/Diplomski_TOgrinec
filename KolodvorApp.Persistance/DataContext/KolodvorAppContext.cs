42d3374366459a1ce33ac792adb574c72ebcda07
Tomislav Ogrinec <togrinec@conet.hr>
2023-05-23 18:00:15 +0200

    Prepare basic persistance structure

---
+++ KolodvorApp.Persistance/DataContext/KolodvorAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KolodvorApp.Persistance.DataContext;
+
+public sealed class KolodvorAppContext : DbContext
+{
+    public KolodvorAppContext(DbContextOptions<KolodvorAppContext> options) : base(options) { }
+
+    //TODO: DbSets
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //TODO: Models
+    }
+}
---
+++ KolodvorApp.Persistance/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using KolodvorApp.Persistance.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KolodvorApp.Persistance;
+
+public static class DbInitializer
+{
+    public static async Task InitializeAsync(IServiceProvider serviceProvider)
+    {
+        using var context = new KolodvorAppContext(serviceProvider.GetRequiredService<DbContextOptions<KolodvorAppContext>>());
+
+        context.Database.SetCommandTimeout(30);
+
+        var applyChanges = false;
+
+        //TODO: seed data checks & method calls
+
+        if (applyChanges) await context.SaveChangesAsync();
+    }
+}
